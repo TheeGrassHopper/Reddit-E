2fce48e566f5332322ecdfdafeb61b6a75abcbff
TheeGrassHopper <almortada88@gmail.com>
2015-07-29 16:13:06 -0700

    app fully functional

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,54 +1,32 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    before_action :find_link
+    before_action :find_comment, only: [:edit, :update, :destroy]
+    before_action  :authenticate_user!
 
   # POST /comments
   # POST /comments.json
   def create
-    @link = Link.find(params[:link_id])
-    @comment = @link.comments.new(comment_params)
+    
+    @comment = @link.comments.create(comment_params)
     @comment.user = current_user
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to link_path(@link), notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
-        format.html { render :new }
+        format.html { redirect_to link_path(@link), notice: 'Must Add A Comment Below!!' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
+  
+
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-      format.html { redirect_to comments_url, notice: 'Comment was successfully updated.' }
+      format.html { redirect_to link_path(@link), notice: 'Comment was successfully updated.' }
       format.json { render :show, status: :created, location: @comment }
       else
                 format.html { render :new }
@@ -62,15 +40,19 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to link_path(@link), notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_comment
+    def find_comment
       @comment = Comment.find(params[:id])
+
+    end
+    def find_link
+      @link = Link.find(params[:link_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,6 @@ class Link < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
 	has_many :comments
+	validates :title , presence: true
+	validates :url , presence: true
 end
